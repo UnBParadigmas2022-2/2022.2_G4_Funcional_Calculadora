e7d7e4e4b30e18f1e4b81772436991500139f08d
Leonardo da Silva Gomes <leonardodasigomes@gmail.com>
2022-11-22 16:46:35 -0300

    Adjust order to calculate
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main (main) where
 import Operation
 
 main :: IO ()
-main = print(calcula "200-2+4")
+main = print(calcula(reverse "200 - 2 + 4"))
--- src/Operation.hs
+++ src/Operation.hs
@@ -4,10 +4,10 @@ import Data.Char()
 import Parser(toList, strToNumber, getNumber)
 
 operacao :: (Int, [Char]) -> Int
-operacao (_, []) = 0
+operacao (num, []) = num
 operacao (num, str) 
-    | h == '+'  = num + calcula t
-    | h == '-'  = num - calcula t
+    | h == '+'  = calcula(t) + num
+    | h == '-'  = calcula(t) - num
     | otherwise = operacao(num, t)
     where h = head str
           t = tail str
@@ -17,6 +17,6 @@ calcula str = resultado(str)
 
 resultado :: ([Char]) -> Int
 resultado ([]) = 0
-resultado (str) = operacao(strToNumber(getNumber(h, t)), str)
+resultado (str) = operacao(strToNumber(reverse(getNumber(h, t))), t)
     where h = head str
           t = tail str
