c08e54a165f4b8fa579b8b2786d3708fc08d4dba
Leonardo da Silva Gomes <leonardodasigomes@gmail.com>
2022-11-22 16:20:41 -0300

    Adjust operacao e resultado params
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- src/Operation.hs
+++ src/Operation.hs
@@ -3,30 +3,20 @@ module Operation(calcula) where
 import Data.Char()
 import Parser(toList, strToNumber, getNumber)
 
--- toList :: Char -> [Char]
--- toList a = [a]
-
--- strToNumber :: [Char] -> Int
--- strToNumber str =  read str :: Int
-
--- getNumber :: (Char, [Char]) -> [Char]
--- getNumber (char, []) = toList char
--- getNumber (char, str) 
---     | h /= '+' && h /= '-' = toList char ++ getNumber(h, t)
---     | otherwise            = toList char
---     where h = head str
---           t = tail str
-
-operacao :: (Int, Char, [Char]) -> Int
-operacao (_, _, []) = 0
-operacao (num, h, t) 
+operacao :: (Int, [Char]) -> Int
+operacao (_, []) = 0
+operacao (num, str) 
     | h == '+'  = num + calcula t
     | h == '-'  = num - calcula t
-    | otherwise = operacao(num, head t, tail t)
+    | otherwise = operacao(num, t)
+    where h = head str
+          t = tail str
 
 calcula :: [Char] -> Int
-calcula c = resultado(head c, tail c)
+calcula str = resultado(str)
 
-resultado :: (Char, [Char]) -> Int
-resultado (h, []) = strToNumber(toList(h))
-resultado (h, t) = operacao(strToNumber(getNumber(h, t)), head t, tail t)
+resultado :: ([Char]) -> Int
+resultado ([]) = 0
+resultado (str) = operacao(strToNumber(getNumber(h, t)), str)
+    where h = head str
+          t = tail str
