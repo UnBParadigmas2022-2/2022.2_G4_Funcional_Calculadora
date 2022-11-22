89b14b0fd9f88b0cc449bdbfc94f2a9df446d196
Leonardo da Silva Gomes <leonardodasigomes@gmail.com>
2022-11-22 12:18:38 -0300

    Create structure to calculater

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,28 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (calcula) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char()
+
+arrayHead :: Char -> [Char]
+arrayHead a = [a]
+
+strToNumber :: [Char] -> Int
+strToNumber str =  read str :: Int
+
+getNumber :: (Char, [Char]) -> [Char]
+getNumber (char, []) = arrayHead(char)
+getNumber (char, array) = if (head array /= '+')
+                        then concat[arrayHead(char), getNumber(((head array), (tail array)))]
+                        else arrayHead(char)
+
+soma :: (Int, Char, [Char]) -> Int
+soma (int, h, []) = 0
+soma (int, h, t) = if ( h == '+' ) 
+                    then int + calcula t
+                    else soma(int, head t, tail t)
+
+calcula :: [Char] -> Int
+calcula c = resultado(head c, tail c)
+
+resultado :: (Char, [Char]) -> Int
+resultado (h, []) = strToNumber(arrayHead(h))
+resultado (h, t) = soma(strToNumber(getNumber(h, t)), head t, tail t)
