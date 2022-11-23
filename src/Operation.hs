ca73be5053d1a929575e93051074c93127be6d40
Amanda Nobre <amandapnobre@gmail.com>
2022-11-23 19:49:27 -0300

    Add exponential function Co-authored-by: Jaime Juan <jaime_ruan@hotmail.com>

--- src/Operation.hs
+++ src/Operation.hs
@@ -1,7 +1,17 @@
-module Operation(calculate) where
+module Operation(calculate, exponential) where
 
 import Data.Char()
 import Parser(toList, strToNumber, getNumber)
+
+
+exponential :: (Int, [Char]) -> [Char]
+exponential (num, []) = reverse(show(num))
+exponential (num, str)
+    | h == '+' || h == '-' || h == '*' || h == '/' = reverse(show(num)) ++ toList(h) ++ exponential(strToNumber(reverse(getNumber(head t, tail t))), t)
+    | h == '^'                                     = exponential(strToNumber(reverse(getNumber(head t, tail t))) ^ num, t)
+    | otherwise                                    = exponential(num, t)
+    where h = head str
+          t = tail str
 
 multiplication :: (Int, [Char]) -> [Char]
 multiplication (num, []) = reverse(show(num))
@@ -26,7 +36,8 @@ calculate :: [Char] -> Int
 calculate str = result(reverse str)
 
 result :: ([Char]) -> Int
-result ([]) = 0
-result (str) = operation(strToNumber(reverse(getNumber(h, t))), multiplication(strToNumber(reverse(getNumber(h, t))), t))
-    where h = head str
-          t = tail str
+result ([])  = 0
+result (str) = operation(strToNumber(reverse(getNumber(h, t))), multiplication(strToNumber(reverse(getNumber(h, t))), 
+               exponential(strToNumber(reverse(getNumber(h, t))),t)))
+    where h  = head str
+          t  = tail str
