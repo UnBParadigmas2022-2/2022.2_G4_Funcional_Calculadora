fa63592be925b54caf54c853f082e37a6ba49f1f
micaellagouveia <micaella2212@gmail.com>
2022-11-23 21:02:55 -0300

    feat: add interactive main

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,22 @@ module Main (main) where
 import Operation(calculate)
 
 main :: IO ()
-main = print(calculate("2 ^ 3 * 1 + 1"))
+main = read_
+
+read_ :: IO()
+read_ = do
+  putStrLn "=========Calculadora========="
+  putStrLn "Insira abaixo a expressão que você deseja calcular:"
+  expr <- getLine
+  
+  if expr == ""
+    then closeProgram
+    else do 
+        print(expr ++ " = " ++ show(calculate(expr)))
+        read_
+
+closeProgram :: IO()
+closeProgram = do
+  print "Parando graciosamente..."
+  print ""
+  print "Obrigado por usar!"
