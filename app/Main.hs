409d3b0bca84160f6a3d22a1ade15689556d2ee9
Hugo Sobral <hugosobral10@gmail.com>
2022-11-24 13:50:41 -0300

    Change project execution

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,6 @@
 module Main (main) where
 
-import Operation(calculate)
-import System.Exit
+import Run(run)
 
 main :: IO ()
-main = read_
-
-read_ :: IO()
-read_ = do
-  putStrLn "=========Calculadora========="
-  putStrLn "Insira abaixo a expressão que você deseja calcular (Pressione Enter para sair):"
-  expr <- getLine
-
-  if expr == ""
-    then closeProgram
-    else return()
-
-  putStrLn ""
-  print("A expressao digitada foi: " ++ expr)
-  putStrLn "Tem certeza que deseja calculá-la? (Verifique se ela está correta)"
-  putStrLn "Digite S ou N"
-  confirmation <- getLine
-
-  if confirmation == "N" ||  confirmation == "n"
-    then read_
-    else return()
-
-  putStrLn ""
-  putStrLn "---------Resultado---------"
-  print("" ++ expr ++ " = " ++ show(calculate(expr)) ++ "")
-  putStrLn "---------------------------"
-  putStrLn ""
-  read_
-
-closeProgram :: IO()
-closeProgram = do
-  putStrLn "Parando graciosamente..."
-  putStrLn ""
-  putStrLn "Obrigado por usar!"
-  exitWith ExitSuccess
+main = run
---
+++ src/Run.hs
@@ -0,0 +1,48 @@
+module Run(run) where
+
+import Operation(calculate)
+import System.Exit
+
+run :: IO()
+run = do
+    printHeader
+    read_
+
+printHeader :: IO()
+printHeader = do 
+  putStrLn "=========Calculadora========="
+  putStrLn "Para utilizar a calculadora, basta digitar a expressão que você deseja calcular."
+  putStrLn "Caso você queira fechar o programa, basta apertar ENTER."
+  putStrLn ""
+  read_
+
+read_ :: IO()
+read_ = do
+  putStrLn "---------------------------"
+  putStrLn ""
+  putStrLn "Insira abaixo a expressão, ou ENTER para sair:"
+  expr <- getLine
+
+  if expr == ""
+    then closeProgram
+    else return()
+
+  putStrLn ""
+  putStrLn("A expressao digitada foi: " ++ expr)
+  putStrLn "Tem certeza que deseja calculá-la? (S/n)"
+  confirmation <- getLine
+
+  if confirmation == "N" ||  confirmation == "n"
+    then read_
+    else return()
+
+  putStrLn ""
+  putStrLn "---------Resultado---------"
+  print("" ++ expr ++ " = " ++ show(calculate(expr)) ++ "")
+  read_
+
+closeProgram :: IO()
+closeProgram = do
+  putStrLn "Parando..."
+  putStrLn "---------------------------"
+  exitWith ExitSuccess
