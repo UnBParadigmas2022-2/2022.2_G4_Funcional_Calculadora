262c63ab58d40d39ecc800846d24be332270331a
micaellagouveia <micaella2212@gmail.com>
2022-11-23 22:15:19 -0300

    feat: add confirmation step on interactive main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Operation(calculate)
+import System.Exit
 
 main :: IO ()
 main = read_
@@ -8,17 +9,33 @@ main = read_
 read_ :: IO()
 read_ = do
   putStrLn "=========Calculadora========="
-  putStrLn "Insira abaixo a expressão que você deseja calcular:"
+  putStrLn "Insira abaixo a expressão que você deseja calcular (Pressione Enter para sair):"
   expr <- getLine
-  
+
   if expr == ""
     then closeProgram
-    else do 
-        print(expr ++ " = " ++ show(calculate(expr)))
-        read_
+    else return()
+
+  putStrLn ""
+  print("A expressao digitada foi: " ++ expr)
+  putStrLn "Tem certeza que deseja calculá-la? (Verifique se ela está correta)"
+  putStrLn "Digite S ou N"
+  confirmation <- getLine
+
+  if confirmation == "N" ||  confirmation == "n"
+    then read_
+    else return()
+
+  putStrLn ""
+  putStrLn "---------Resultado---------"
+  print("" ++ expr ++ " = " ++ show(calculate(expr)) ++ "")
+  putStrLn "---------------------------"
+  putStrLn ""
+  read_
 
 closeProgram :: IO()
 closeProgram = do
-  print "Parando graciosamente..."
-  print ""
-  print "Obrigado por usar!"
+  putStrLn "Parando graciosamente..."
+  putStrLn ""
+  putStrLn "Obrigado por usar!"
+  exitWith ExitSuccess
